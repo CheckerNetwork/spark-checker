66a2d4a493d8f49562b2cf51da8ccdb22003818e
Nikolas Haimerl <nikolas@meridian.space>
2025-03-10 17:45:10 +0100

    resolve PR threads

--- test/smart-contract-client.test.js
+++ test/smart-contract-client.test.js
@@ -97,5 +97,5 @@ test('getMinerPeerId returns correct peer id for miner f03303347', async () => {
   const peerId = await getMinerPeerId('f03303347')
 
   assert(typeof peerId === 'string', 'Expected peerId to be a string')
-  assertEquals(peerId, '12D3KooWJ91c6xQshrNe7QAXPFAaeRrHWq2UrgXGPf8UmMZMwyZ5')
+  assertEquals(peerId, '12D3KooWCtiN7tAjeLKL4mashteXdH4htUrzWu8bWN9kDU3qbKjQ')
 })
