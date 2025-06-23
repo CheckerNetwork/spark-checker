67bcdd83f376777085e204c5281450f3021c27df
Miroslav Bajtoš <oss@bajtos.net>
2025-06-23 13:25:57 +0200

    feat! shut down
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import Spark from './lib/spark.js'
+Zinnia.activity.error(
+  'Filecoin Station and Checker Network programmes ended. You can uninstall your node now. Thank you for your participation!',
+)
 
-const spark = new Spark()
-await spark.run()
+// import Spark from './lib/spark.js'
+// const spark = new Spark()
+// await spark.run()
