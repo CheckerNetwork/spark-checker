a64669e0e8365ac2136b80b88c051aa8eaff9ec9
Miroslav Bajtoš <oss@bajtos.net>
2025-06-24 12:42:57 +0200

    fixup! add infinite no-op loop
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ Zinnia.activity.error(
   'Filecoin Station and Checker Network programmes ended. You can uninstall your node now. Thank you for your participation!',
 )
 
+while (true) {
+  await new Promise((resolve) => setTimeout(resolve, 60_000))
+}
+
 // import Spark from './lib/spark.js'
 // const spark = new Spark()
 // await spark.run()
