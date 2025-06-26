483b81dfe920395927010542f280620d16d27d15
Miroslav Bajtoš <oss@bajtos.net>
2025-06-26 17:53:59 +0200

    feat! shut down (#140)
    * feat! shut down
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * fixup! eslint - missing Zinnia global
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * fixup! add infinite no-op loop
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * fixup! update message
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * fixup! revert unintended changes
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * fixup! prefix the message to trigger offline mode
    
    In order to trigger offline mode in Checker Desktop and show offline
    status icon in the tray menu, the error message has to start with the
    prefix `Spark ` (the space character is important).
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    ---------
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-import Spark from './lib/spark.js'
+/* global Zinnia */
 
-const spark = new Spark()
-await spark.run()
+Zinnia.activity.error(
+  'Spark update: Filecoin Station and Checker Network programmes ended. The node is no longer contributing to the network, and there will be no further rewards. Thank you for your participation!',
+)
+
+while (true) {
+  await new Promise((resolve) => setTimeout(resolve, 60_000))
+}
+
+// import Spark from './lib/spark.js'
+// const spark = new Spark()
+// await spark.run()
